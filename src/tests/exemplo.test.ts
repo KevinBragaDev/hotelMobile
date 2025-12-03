f699da454f7eea631290b49e656ec92dd4ea13d9
KEVIN CHRISTIAN DE OLIVEIRA BRAGA <kevin.cobraga@senacsp.edu.br>
2025-12-03 14:33:58 -0300

    feat: arquivo para teste automatizado

---
+++ src/tests/exemplo.test.ts
@@ -0,0 +1,9 @@
+test("exemplo", () => {
+  const nome = "felipe teste da silva";
+  expect(nome).toBe("felipe amaral de souza");
+})
+
+test("exemplo2", () => {
+  const result = 5+5;
+  expect(result).toBe(10);
+})
